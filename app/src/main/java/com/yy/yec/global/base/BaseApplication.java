46e8afec39c7725c7ef7d40a57f3fa08f2b97dfd
472708969@qq.com <472708969@qq.com>
2017-02-06 16:19:19 +0800

    update

--- app/src/main/java/com/yy/yec/global/base/BaseApplication.java
+++ app/src/main/java/com/yy/yec/global/base/BaseApplication.java
@@ -1,11 +1,11 @@
 package com.yy.yec.global.base;
 
+import android.app.Application;
 import android.content.Context;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
 
 import com.yy.yec.global.manager.AppActivityManager;
 
@@ -14,7 +14,7 @@ import java.io.File;
 /**
  * Created by zzq on 2016/9/24.
  */
-public abstract class BaseApplication extends MultiDexApplication {//dex方法数超过65536问题
+public abstract class BaseApplication extends Application {//dex方法数超过65536问题
     protected static Context mContext;
     protected static Resources mResource;
 
